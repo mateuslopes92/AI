c2d8c4504a0b685d0d47f35eb3fc4b2bcc3c830b
mateuslopes92 <mateuslopes92@gmail.com>
2025-12-23 20:51:24 -0300

    enpoint to predict home prices from api

--- machine_learning/regression_data_science/server/server.py
+++ machine_learning/regression_data_science/server/server.py
@@ -12,6 +12,23 @@ def get_location_names():
 
   return "Hello, World!"
 
+@app.route('/predict_home_price', methods=['POST'])
+def predict_home_price():
+  sqft = float(request.form['total_sqft'])
+  bath = int(request.form['bath'])
+  bhk = int(request.form['bhk'])
+  location = request.form['location']
+
+  estimated_price = util.get_estimated_price(location, sqft, bath, bhk)
+
+  response = jsonify({
+    'estimated_price': estimated_price
+  })
+
+  response.headers.add('Access-Control-Allow-Origin', '*')
+
+  return response
+
 if __name__ == '__main__':
   print("Starting flask server for home price prediction...")
   app.run()
